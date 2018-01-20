edeff052386a579e07ce19ee248dd49b8bfabda1
perry <perry091061@gmail.com>
2018-01-20 16:28:22 +0000

    Basic table view start

---
+++ TableViews/TableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Perry Davies on 20/01/2018.
+//  Copyright © 2018 Perry Davies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
